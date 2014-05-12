b9b1af89101d290483eaecb0d89cc4a8fb7e13b4
jonathonmills <jonathonmills@gmail.com>
2014-05-12 23:15:18 +0100

    Media
    Media removed

--- media/realia/aviators/core/plugins/customizations/customizations.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//function aviators_realia_customize($wp_customize) {
-//    aviators_realia_customize_general($wp_customize);
-//}
-//
-//add_action('customize_register', 'aviators_realia_customize');
